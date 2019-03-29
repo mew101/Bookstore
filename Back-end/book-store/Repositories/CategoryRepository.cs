fec467af8984e1422ffc9b33379fd135f1228a47
Mo <mshwss@gmail.com>
2019-03-29 14:51:49 -0400

    added create to category repo

--- Back-end/book-store/Repositories/CategoryRepository.cs
+++ Back-end/book-store/Repositories/CategoryRepository.cs
@@ -2,10 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookStore.Models;
 
 namespace BookStore.Repositories
 {
     public class CategoryRepository : ICategoryRepository
-{
+    {
+        StoreContext db;
+
+        public CategoryRepository(StoreContext db)
+        {
+            this.db = db;
+        }
+        public Category GetById(int id)
+        {
+            return db.Categorys.Single(Category => Category.CategoryId == id);
+
+        }
+        public IEnumerable<Category> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Create(Category category)
+        {
+            db.Categorys.Add(category);
+            db.SaveChanges();
+        }
+    }
 }
-}
+
--- Back-end/book-store/Repositories/ICategoryRepository.cs
+++ Back-end/book-store/Repositories/ICategoryRepository.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookStore.Models;
 
 namespace BookStore.Repositories
 {
-    public class ICategoryRepository
-{
-}
+    public interface  ICategoryRepository
+    {
+        Category GetById(int id);
+        IEnumerable<Category> GetAll();
+        void Create(Category category);
+    }
 }
