eff640fe4047b714a120d5613ed05c4ed6d7f7b4
khanishvili <khnshvl@gmail.com>
2019-03-31 21:13:06 -0400

    Delete Method is working

--- Front-end/src/Book.js
+++ Front-end/src/Book.js
@@ -1,15 +1,21 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import './Book.css';
 
 export default class Book extends Component {
+  deleteBook = () => {
+    fetch(`https://localhost:44353/api/Book/${this.props.bookId}`, {
+      method: 'DELETE'
+    });
+  };
   render() {
     const { bookId, name, detailDescription, author } = this.props;
 
     return (
       <div className="book">
-       <h3>{name}</h3>
+        <h3>{name}</h3>
         <p>{author}</p>
         <p>{detailDescription}</p>
+        <button onClick={this.deleteBook}>Delete</button>
       </div>
     );
   }
